50b5c23c37e8a199adbb12e0852edfd6f2de0b0a
Peri <periha@gmail.com>
2016-12-16 12:58:56 -0500

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 main(){
-  cout << "hello world\n";
+  string input;
+  
+  cout << "Say somthing: ";
+  cin >> input;
+  if(input.size()>0)
+    cout << "hello world\n";
+  else
+    return 0;
  }
